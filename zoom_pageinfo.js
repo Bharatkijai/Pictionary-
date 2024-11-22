b41a2c84b6b9b489be412dba60d29ca6de787e02
Bharatkijai <161074895+Bharatkijai@users.noreply.github.com>
2024-11-22 22:34:46 +0530

    Add files via upload

---
+++ zoom_pageinfo.js
@@ -0,0 +1,8 @@
+pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null]];
+pagedata = [ ["./howtoplay.html","HOW TO PLAY","Divide into Teams Split players into two or more teams. Each team needs at least two players (one as the Picturist and others as guessers).  ...",""],
+["./introduction.html","GAME KIT","This game includes the following items:  Game board Drawing pad Marker Sand timer Category cards Dice Eraser",""],
+["./rules.html","RULES","Teams: Players are divided into teams, each with a Picturist.",""],
+["./welcome-topic.html","OBJECTIVE","The objective of the board game Pictionary is to guess words or phrases based on drawings created by teammates within a limited amount of time. Th...",""]];
