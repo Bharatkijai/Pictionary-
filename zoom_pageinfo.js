bd97f96703b1d8c7181ec5f9b65dc019485b48b9
Bharatkijai <161074895+Bharatkijai@users.noreply.github.com>
2024-11-23 13:56:55 +0530

    Add files via upload

--- zoom_pageinfo.js
+++ zoom_pageinfo.js
@@ -1,8 +1,14 @@
 pageinfo = [[0,0,0,0,null],
 	[0,0,0,0,null],
 	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
 	[0,0,0,0,null]];
-pagedata = [ ["./howtoplay.html","HOW TO PLAY","Divide into Teams Split players into two or more teams. Each team needs at least two players (one as the Picturist and others as guessers).  ...",""],
+pagedata = [ ["./gamekit.html","GAME KIT","This game includes the following items:  Game board Drawing pad Marker Sand timer Category cards Dice Eraser",""],
+["./howtoplay.html","HOW TO PLAY","Divide into Teams Split players into two or more teams. Each team needs at least two players (one as the Picturist and others as guessers).  ...",""],
 ["./introduction.html","GAME KIT","This game includes the following items:  Game board Drawing pad Marker Sand timer Category cards Dice Eraser",""],
+["./objective.html","OBJECTIVE","The objective of the board game Pictionary is to guess words or phrases based on drawings created by teammates within a limited amount of time. Th...",""],
 ["./rules.html","RULES","Teams: Players are divided into teams, each with a Picturist.",""],
-["./welcome-topic.html","OBJECTIVE","The objective of the board game Pictionary is to guess words or phrases based on drawings created by teammates within a limited amount of time. Th...",""]];
+["./welcome-topic.html","OBJECTIVE","The objective of the board game Pictionary is to guess words or phrases based on drawings created by teammates within a limited amount of time. Th...",""],
+["./welcome.html","WELCOME","Pictionary is a lively and creative guessing game that brings people together through drawings and quick thinking. The rules are simple: one playe...",""]];
